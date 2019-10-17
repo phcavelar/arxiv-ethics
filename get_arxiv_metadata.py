68d2a7ce9fba36b8b896f6ba8f6b949db59362b3
phcavelar <9154669+phcavelar@users.noreply.github.com>
2019-10-17 16:10:56 -0300

    Script to scrape relevant arxiv metadata from the papers in the csv file

--- get_arxiv_metadata.py
+++ get_arxiv_metadata.py
@@ -1,17 +1,39 @@
-import csv, sys, re, time, urllib.request
+import csv, json, sys, re, time, requests
 from lxml import etree
 
 csvfname = sys.argv[1]
-newcsvfname = sys.argv[2]
+newjsonfname = sys.argv[2]
 
-BLOCK_SIZE = 512
+text_tags = [
+        "id",
+        "published",
+        "title",
+        "summary",
+]
+xml_tags = {
+        "primary_category":"term",
+        "category":"term"
+}
+
+single_tags = [
+        "id",
+        "published",
+        "title",
+        "summary",
+        "primary_category",
+]
+list_tags = [
+        "category"
+]
+
+BLOCK_SIZE = 10
 
 URL_BASE = "http://export.arxiv.org/api/query?id_list="
 
 with open(csvfname, mode="r", encoding="utf-8", newline='') as csvf:
-    with open(newcsvfname, mode="w", encoding="utf-8", newline='') as ncsvf:
+    with open(newjsonfname, mode="w", encoding="utf-8", newline='') as njsonf:
         reader = csv.reader(csvf, delimiter='\t', quotechar='"')
-        writer = csv.writer(ncsvf, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+
         arxiv_ids = []
         for i, row in enumerate(reader):
             title = row[1]
@@ -24,21 +46,41 @@ with open(csvfname, mode="r", encoding="utf-8", newline='') as csvf:
             #end if
         #end for
         arxiv_ids = arxiv_ids[1:]
-        data_xmls = []
-        for i in range(0,len(arxiv_ids),BLOCK_SIZE):
-            url = URL_BASE+(",".join(arxiv_ids[i:i+BLOCK_SIZE]))
-            print(url)
-            data = urllib.request.urlopen(url).read()
-            data_xmls.append(data)
-            print(data)
-            break
-            time.sleep(1.0)
+        papers = []
+        try:
+            with requests.Session() as sess:
+                for i in range(0,len(arxiv_ids),BLOCK_SIZE):
+                    url = URL_BASE+(",".join(arxiv_ids[i:i+BLOCK_SIZE]))
+                    xml = requests.get(url).content
+                    
+                    tree = etree.ElementTree(etree.XML(xml))
+                    root = tree.getroot()
+                    for entry in [e for e in root if "entry" in e.tag]:
+                        edict = {
+                                **{t:None for t in single_tags},
+                                **{t:[] for t in list_tags}
+                        }
+                        for value in entry:
+                            tag = value.tag
+                            tag = tag.replace("{http://www.w3.org/2005/Atom}","").replace("{http://arxiv.org/schemas/atom}","")
+                            if tag in text_tags:
+                                entry_value = value.text
+                            elif tag in xml_tags:
+                                entry_value = value.get(xml_tags[tag])
+                            if tag in single_tags:
+                                edict[tag] = entry_value
+                            elif tag in list_tags:
+                                edict[tag] += [entry_value]
+                        papers.append(edict)
+                        print("{i}: {aid} {title}".format(i=len(papers), aid=edict["id"][-10:], title=edict["title"][:60].replace("\n","")))
+                    #end for entry
+                    
+                    time.sleep(1.01)
+                #end for
+            #end with
+        except KeyboardInterrupt:
+            pass
         
-        for xml in data_xmls:
-            tree = etree.ElementTree(etree.XML(xml))
-            root = tree.getroot()
-            print(root)
-            help(etree)
-            
+        json.dump(papers, njsonf, ensure_ascii=False)
     #end with
 #end with
