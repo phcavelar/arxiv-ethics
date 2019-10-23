9b3cbd71846ea1008b83881ade2319c24ea404d5
phcavelar <9154669+phcavelar@users.noreply.github.com>
2019-10-23 14:38:48 -0300

    Added code to start scraping one month before the initial month of the last record

--- fetch_arxiv.py
+++ fetch_arxiv.py
@@ -1,8 +1,17 @@
-import json
+import json, os, datetime
 from sickle import Sickle
 
+earliest_date = datetime.date(1,2,1)
+for fname in [fname for fname in os.listdir("jsons/") if fname != ".gitkeep"]:
+    with open("jsons/{}".format(fname),"r") as f:
+        d = json.load(f)
+        ddate = datetime.date.fromisoformat(d["date"][0])
+        earliest_date = max(earliest_date, ddate)
+
+earliest_date = datetime.date(earliest_date.year,earliest_date.month-1,earliest_date.day)
+
 sickle = Sickle('http://export.arxiv.org/oai2')
-records = sickle.ListRecords(metadataPrefix='oai_dc')
+records = sickle.ListRecords(metadataPrefix='oai_dc',**{"from":earliest_date})
 
 for record in records:
     metadata = record.metadata
